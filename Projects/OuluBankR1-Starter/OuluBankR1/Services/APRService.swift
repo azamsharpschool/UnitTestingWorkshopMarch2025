2b7e597930b147d004fe522424eaa5aab29c8a8f
Mohammad Azam <azamsharp@gmail.com>
2025-03-01 11:25:42 -0600

    adding code for APRService

--- Projects/OuluBankR1-Starter/OuluBankR1/Services/APRService.swift
+++ Projects/OuluBankR1-Starter/OuluBankR1/Services/APRService.swift
@@ -7,6 +7,62 @@
 
 import Foundation
 
+enum CreditScoreServiceError: Error, LocalizedError {
+    case noCreditScoreFound
+    
+    // Provide localized error descriptions
+    var errorDescription: String? {
+        switch self {
+        case .noCreditScoreFound:
+            return NSLocalizedString(
+                "No credit score was found for the provided SSN.",
+                comment: "Error message when no credit score is found."
+            )
+       
+        }
+    }
+}
 
+enum APRServiceError: Error, LocalizedError {
+    
+    case invalidSSN
 
+    // Provide localized error descriptions
+    var errorDescription: String? {
+        switch self {
+        case .invalidSSN:
+            return NSLocalizedString(
+                "The provided SSN is invalid. Please enter a valid SSN.",
+                comment: "Error message when the SSN is invalid."
+            )
+        }
+    }
+}
 
+struct APRService {
+    
+    let creditScoreService: CreditScoreService
+    
+    func getAPR(ssn: String) async throws -> Double {
+        
+        if !ssn.isSSN {
+            throw APRServiceError.invalidSSN
+        }
+        
+        guard let creditScore = try await creditScoreService.getCreditScore(ssn: ssn) else {
+            throw CreditScoreServiceError.noCreditScoreFound
+        }
+        
+        // calculate the apr based on credit score
+        if let score = creditScore.score {
+            if score > 650 {
+                return Double.random(in: 1...3)
+            } else {
+                return Double.random(in: 6...10)
+            }
+        } else {
+            throw CreditScoreServiceError.noCreditScoreFound
+        }
+    }
+    
+}
